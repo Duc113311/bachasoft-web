055207bb689726c4d26ecc59e851f339eaa53287
Duc Nguyen <ducnv@bachasoft.com>
2025-02-25 23:55:34 +0700

    cap nhat ui

--- js/zoom.js
+++ js/zoom.js
@@ -1,16 +1,26 @@
-const modal = document.getElementsByClassName('idMyModal');
-const img = document.getElementsByClassName('toZoom');
-const modalImg = document.getElementsByClassName('modal-content');
-for ( let i = 0; i < img.length; i++ ) {
-  img[i].onclick = function () {
-    modal[i].style.display = "block";
-    modalImg[i].src = this.src;
-  }
-}
+// Lấy phần tử modal
+const modal = document.querySelector('.idMyModal');
+const modalImg = document.querySelector('.modal-content');
+const closeBtn = document.querySelector('.close');
 
-var span = document.getElementsByClassName("close");
-for ( let i = 0; i < span.length; i++ ) {
-  span[i].onclick = function() { 
-    modal[i].style.display = "none";
+// Lấy tất cả các ảnh có class "toZoom"
+const images = document.querySelectorAll('.toZoom');
+
+images.forEach(img => {
+  img.addEventListener('click', function () {
+    modal.style.display = "block"; // Hiển thị modal
+    modalImg.src = this.src; // Gán ảnh được click vào modal
+  });
+});
+
+// Đóng modal khi click vào nút close
+closeBtn.addEventListener('click', function () {
+  modal.style.display = "none";
+});
+
+// Đóng modal khi click ra ngoài
+modal.addEventListener('click', function (e) {
+  if (e.target === modal) {
+    modal.style.display = "none";
   }
-}
+});
